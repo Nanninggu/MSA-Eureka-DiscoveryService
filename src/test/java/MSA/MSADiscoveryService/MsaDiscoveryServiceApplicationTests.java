f948ed853d7ad82870f230362755a79791934673
seungkim <seungkim@hist.co.kr>
2023-03-30 17:10:33 +0900

    MSA-Spring-Eureka-DiscoveryService-Code

---
+++ src/test/java/MSA/MSADiscoveryService/MsaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package MSA.MSADiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
